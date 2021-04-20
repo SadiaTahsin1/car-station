e824c79d79cb94e2a85fe44a8eafb7c6cac9f4b3
Sadia Tahsin1 <sadiaabedinctg@gmail.com>
2021-04-20 23:59:57 +0600

    ok

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div style={{height:"200px",backgroundColor:"purple",textAlign:"center",margin:"2%",padding:"2%"}}>
-            Lorem ipsum dolor sit amet.
+          Contact no: 013444444
         </div>
     );
 };
